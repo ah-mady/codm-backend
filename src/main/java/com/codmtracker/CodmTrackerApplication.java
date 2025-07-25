4473a41f53e407a2566cd1d4641f4956e2d24207
Ahmed <ahmad_bhai28@yahoo.com>
2025-07-25 22:02:38 +0200

    initializing project

---
+++ src/main/java/com/codmtracker/CodmTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.codmtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodmTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodmTrackerApplication.class, args);
+    }
+
+}
